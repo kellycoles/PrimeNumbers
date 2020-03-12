9446413590a7ba530f804991feeb1d0e914a60d3
kellycoles <kellycoles1@gmail.com>
2020-03-11 21:14:03 -0500

    nested for loops to check test for prime

--- Program.cs
+++ Program.cs
@@ -17,6 +17,23 @@ namespace PrimeNumbers
 
             // Read the target number and convert to integer
             int target = Int32.Parse(Console.ReadLine());
+
+            // 1 is neither prime nor composite. So start at 2
+            for (int i = 2; i <= target; i++)
+            {
+                for (j = 2; j < i; j++)
+                {
+                    // A number is not prime if it is divisible by any other number,
+                    // other than 1 and itself.
+                    if (i % j == 0)
+                    {
+                        isNumberComposite = true;
+                        // We can break out of the inner for loop as we know the number
+                        // is not prime
+                        break;
+                    }
+                }
+            }
         }
     }
 }
