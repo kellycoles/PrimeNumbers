8c2cd92c47ab8b4526bbc964f0046dfb62090102
kellycoles <kellycoles1@gmail.com>
2020-03-11 20:58:31 -0500

    prompt user for number, read input

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace PrimeNumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Declare a boolean variable to determine is if a number is prime
+            bool isNumberComposite = false;
+            int j;
+
+
+            // Prompt the user to enter their target number
+            Console.WriteLine("Enter your Target?");
+
+
+            // Read the target number and convert to integer
+            int target = Int32.Parse(Console.ReadLine());
         }
     }
 }
